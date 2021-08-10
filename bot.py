bfdebf7b5eb3c2a9effa9de2711a6e5b8bd4092e
Michael5Kolesnikov <86573517+Michael5Kolesnikov@users.noreply.github.com>
2021-08-10 23:27:07 +0300

    Structural change
    Сhanged the structure of buttons, added storage of user data, long polling replaced by webhook

--- bot.py
+++ bot.py
@@ -1,22 +1,39 @@
 import telebot
-import config
 from telebot import types
+from flask import Flask, request
+
+import os
 import random
 
-bot = telebot.TeleBot(config.TOKEN)
-events = []  # события пользователя
-event = {}  # переменная для вносимых событий
-notes = []  # заметки пользователя
-notes_name = []  # названия заметок пользователя
+# настройка webhook
+from config import TOKEN, secret, url
+
+bot = telebot.TeleBot(TOKEN, threaded=False)
+bot.remove_webhook()
+bot.set_webhook(url=url)
+app = Flask(__name__)
+
+
+@app.route('/' + secret, methods=['POST'])
+def webhook():
+    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
+    bot.process_new_updates([update])
+    return 'ok', 2000
+
+
+# Обработка сообщений
+main_buttons = [types.KeyboardButton(button) for button in ('События', 'Заметки', '🎲 Рандомное число', '😊 Как дела?', 'Мои возможности')]
 
 
 @bot.message_handler(commands=['start'])  # обработка сообщения '/start' при первом запуске. Пявление главных кнопок
-def welcome(message):
+def welcome(message: types.Message):
+    id_ = message.from_user.id
+    # если пользователь первый раз написал команду '/start', то добавляем его id в список_id (это Data_clients) наших пользователей
+    if f'{id_}' not in os.listdir('Data_clients'):
+        os.mkdir(f'Data_clients//{id_}')
+        os.mkdir(f'Data_clients//{id_}//События')
+        os.mkdir(f'Data_clients//{id_}//Заметки')
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    main_buttons = [types.KeyboardButton(button) for button in ('Новое событие', 'Список событий',
-                                                                'Новая заметка', 'Список заметок', 'Вызвать заметку',
-                                                                '🎲 Рандомное число', '😊 Как дела?',
-                                                                'Мои возможности')]
     markup.add(*main_buttons)
     bot.send_message(message.chat.id,
                      f'Приветствую тебя, {message.from_user.first_name}!\nЯ - {bot.get_me().first_name}, бот, созданный, чтобы быть тебе полезным.',
@@ -24,37 +41,18 @@ def welcome(message):
 
 
 @bot.message_handler(content_types=['text'])
-def answer(message):
-    global events
+def answer(message: types.Message):
     if message.chat.type == 'private':
-        if message.text == 'Новое событие':
-            msg = bot.send_message(message.chat.id, 'Введите дату в формате:\nдень:месяц:год')
-            bot.register_next_step_handler(msg, date)
-
-        elif message.text == 'Список событий':
-            bot.reply_to(message, 'Ваш список событий:')
-            all_items = ''
-            for event_ in events:
-                for item in event_:
-                    all_items += f'{item} : {event_[item]}' + '\n'
-                bot.send_message(message.chat.id, all_items)
-                all_items = ''
-
-        elif message.text == 'Новая заметка':
-            msg = bot.send_message(message.chat.id, 'Введите название заметки:')
-            bot.register_next_step_handler(msg, name_note)
-
-        elif message.text == 'Список заметок':
-            bot.send_message(message.chat.id, '\n'.join(notes_name))
-
-        elif message.text == 'Вызвать заметку':
-            markup = types.InlineKeyboardMarkup(row_width=2)
-            notes_buttons = [types.InlineKeyboardButton(button, callback_data=str(notes_name.index(button))) for button in notes_name]
-            markup.add(*notes_buttons)
-            bot.send_message(message.chat.id, 'Какую заметку хотите просмотреть?', reply_markup=markup)
+        if message.text == 'События' or message.text == 'Заметки':
+            markup = types.InlineKeyboardMarkup(row_width=3)
+            create = types.InlineKeyboardButton('Создать', callback_data=f'create_{message.text}')
+            look = types.InlineKeyboardButton('Посмотреть', callback_data=f'look_{message.text}')
+            delete = types.InlineKeyboardButton('Удалить', callback_data=f'delete_{message.text}')
+            markup.add(create, look, delete)
+            bot.send_message(message.chat.id, 'Что хотите сделать?', reply_markup=markup)
 
         elif message.text == '🎲 Рандомное число':
-            bot.send_message(message.chat.id, str(random.randint(0, 100)))
+            bot.send_message(message.chat.id, str(random.randint(0, 1000)))
 
         elif message.text == '😊 Как дела?':
             markup = types.InlineKeyboardMarkup(row_width=2)
@@ -64,83 +62,149 @@ def answer(message):
             bot.send_message(message.chat.id, 'Отлично, сам как?', reply_markup=markup)
 
         elif message.text == 'Мои возможности':
-            bot.send_message(message.chat.id, '1. Создание события – дата, время, заголовок события, описание события, время напоминания \n'
-                                              '2. Напоминание о событие – выводится время события и описание события\n'
-                                              '3. Вызов списка запланированных событий (дата, время, заголовки)\n'
-                                              '4. Создание заметок\n'
-                                              '5. Вызов списка заметок (заголовки)\n'
-                                              '6. Вызов отдельной заметки\n'
-                                              '7. Сгенерировать случайное число от 0 до 100')
+            bot.send_message(message.chat.id,
+                             '1. Создание/удаление события – дата, время, заголовок события, описание события, время напоминания \n'
+                             '2. Напоминание о событие – выводится время события и описание события\n'
+                             '3. Вызов списка запланированных событий (дата, время, заголовки)\n'
+                             '4. Создание/удаление заметок\n'
+                             '5. Вызов списка заметок (заголовки)\n'
+                             '6. Вызов отдельной заметки\n'
+                             '7. Сгенерировать случайное число от 0 до 1000')
 
         else:
             bot.send_message(message.chat.id, 'Я не знаю что ответить 😢')
 
 
-# работа с заметками
-def name_note(message):
-    global notes_name
-    if message.text not in notes_name:
-        notes_name.append(message.text)
+path = ''  # для путей создаваемых файлов и папок
+kind = ''  # для разделения событий и заметок между собой
+
+
+def write_down(path_, text):
+    f = open(path_, 'a')
+    f.write(text + '\n')
+    f.close()
+
+
+def name(message: types.Message):  # обработка названий событий и заметок
+    global path, kind
+    id_ = message.from_user.id
+
+    # если события или заметки с таким же названием еще нет, то создаем новую
+    if message.text not in os.listdir(f'Data_clients//{id_}//{kind}'):
+        path = f'Data_clients//{id_}//{kind}//{message.text}.txt'
+        f = open(path, 'w')
+        f.write(message.text + '\n')
+        f.close()
+        if kind == 'События':
+            msg = bot.send_message(message.chat.id, 'Введите дату события:')
+            bot.register_next_step_handler(msg, date)
+        else:
+            msg = bot.send_message(message.chat.id, 'Введите содержание заметки:')
+            bot.register_next_step_handler(msg, description)
     else:
-        bot.send_message(message.chat.id, 'Уже есть заметка с таким именем!')
-        return
-    msg = bot.send_message(message.chat.id, 'Введите содержание заметки:')
-    bot.register_next_step_handler(msg, text_note)
+        bot.send_message(message.chat.id, 'Извините. Вы уже использовали такое название!')
 
 
-def text_note(message):
-    global notes
-    notes.append(message.text)
-
-
-# работа с событиями
-def date(message):
-    global event
-    event = {'Дата': message.text}
-    msg = bot.send_message(message.chat.id, 'Введите время в формате:\nчас:минуты')
+def date(message: types.Message):
+    global path
+    write_down(path, message.text)
+    msg = bot.send_message(message.chat.id, 'Введите время события:')
     bot.register_next_step_handler(msg, time)
 
 
-def time(message):
-    global event
-    event.update({'Время': message.text})
-    msg = bot.send_message(message.chat.id, 'Введите название события:')
-    bot.register_next_step_handler(msg, name)
-
-
-def name(message):
-    global event
-    event.update({'Название': message.text})
-    msg = bot.send_message(message.chat.id, 'Введите описание события: (по желанию)')
+def time(message: types.Message):
+    global path
+    write_down(path, message.text)
+    msg = bot.send_message(message.chat.id, 'Введите описание события:')
     bot.register_next_step_handler(msg, description)
 
 
-def description(message):
-    global event
-    if description:
-        event.update({'Описание': message.text})
-    msg = bot.send_message(message.chat.id, 'Введите время напоминания в формате час:минута:день:месяц:год')
-    bot.register_next_step_handler(msg, time_of_remaining)
+def description(message: types.Message):  # описание события или заметки
+    global path
+    write_down(path, message.text)
+    if kind == 'События':
+        msg = bot.send_message(message.chat.id, 'Введите время напоминания четко в формате год.месяц.день.час.минута\n'
+                                                'Пример - 2021.01.01.09.30\n'
+                                                'Если вам не нужно напоминание, то вы можете пропустить этот шаг написав цифру 0')
+        bot.register_next_step_handler(msg, time_of_remaining)
+    else:
+        bot.send_message(message.chat.id, 'Заметка успешно создана!')
 
 
-def time_of_remaining(message):
-    global event
-    event.update({'Время напоминания': message.text})
-    events.append(event)
+def time_of_remaining(message: types.Message):
+    global path
+    text = message.text
+    time_remain = text[0:4] + text[5:7] + text[8:10] + text[11:13] + text[14:]
+    if time_remain.isdigit() and len(text) == 16:
+        write_down(path, message.text)
+        bot.send_message(message.chat.id, 'Событие успешно создано!')
+        f = open(f'Remain//{time_remain}.txt', 'w')
+        f.close()
+        f = open(path)
+        write_down(f'Remain//{time_remain}.txt', f'{message.chat.id}\n' + f.read())
+        f.close()
+    elif text == '0':
+        bot.send_message(message.chat.id, 'Событие успешно создано!')
+    else:
+        msg = bot.send_message(message.chat.id, 'Извините! Вы неверно указали время напоминания!  Введите, пожалуйста, его еще раз')
+        bot.register_next_step_handler(msg, time_of_remaining)
 
 
 @bot.callback_query_handler(func=lambda call: True)  # работа с кнопками под сообщениями
 def callback_inline(call):
+    global path, kind
     try:
         if call.message:
-            if call.data == 'good':
-                bot.send_message(call.message.chat.id, 'Вот и отличненько 😊')
+            id_ = call.message.chat.id
+            
+            # обработка действий с событиями и заметками
+            if 'create' == call.data[0:6]:
+                msg = bot.send_message(id_, 'Введите название:')
+                kind = call.data[7:]
+                bot.register_next_step_handler(msg, name)
+            elif 'look' == call.data[0:4]:
+                if call.data[5:] == 'События':
+                    kind = 'События'
+                else:
+                    kind = 'Заметки'
+                path = f'Data_clients//{id_}//{kind}'
+                list_ = os.listdir(path)
+                if list_:
+                    markup = types.InlineKeyboardMarkup(row_width=3)
+                    markup.add(*[types.InlineKeyboardButton(button[:-4], callback_data=str(list_.index(button))) for button in list_])
+                    bot.send_message(id_, 'Список:', reply_markup=markup)
+                else:
+                    bot.send_message(id_, 'Тут еще ничего нет')
+            elif 'delete' == call.data[0:6]:
+                path = f'Data_clients//{id_}//{call.data[7:]}'  # с помощью call.data выбираем нужную папку(либо События, либо Заметки)
+                list_ = os.listdir(path)
+                if list_:
+                    markup = types.InlineKeyboardMarkup(row_width=3)
+                    markup.add(*[types.InlineKeyboardButton(button[:-4], callback_data=str(list_.index(button)) + 'd') for button in list_])
+                    # добавляем к callback_data букву d, чтобы потом удалить, а не просмотреть
+                    bot.send_message(id_, 'Что хотите удалить?', reply_markup=markup)
+                else:
+                    bot.send_message(id_, 'Тут еще ничего нет')
+
+            # обработка маленького разговора "Как дела?"
+            elif call.data == 'good':
+                bot.send_message(id_, 'Вот и отличненько 😊')
             elif call.data == 'bad':
-                bot.send_message(call.message.chat.id, 'Бывает 😢')
-            elif call.data.isdigit():  # вызов определенной заметки
-                bot.send_message(call.message.chat.id, notes[int(call.data)])
+                bot.send_message(id_, 'Бывает 😢')
+
+            # вызов или удаление определенного события или заметки
+            elif call.data[0].isdigit():
+                list_ = os.listdir(path)
+                if call.data[-1].isdigit():  # проверяем наличие буквы d
+                    path = f'{path}//{list_[int(call.data)]}'
+                    f = open(path)
+                    bot.send_message(id_, f.read())
+                    f.close()
+                else:
+                    os.remove(f'{path}//{list_[int(call.data[:-1])]}')
+                    bot.send_message(id_, 'Удалено успешно!')
+
+            bot.edit_message_text(chat_id=id_, message_id=call.message.message_id, text=call.message.text, reply_markup=None)  # удаление использованной кнопки
+
     except Exception as e:
         print(repr(e))
-
-
-bot.polling(none_stop=True)  # запуск
