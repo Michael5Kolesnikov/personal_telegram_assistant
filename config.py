ffce9ccbed4030fd0875a0755e54443194446612
Michael5Kolesnikov <86573517+Michael5Kolesnikov@users.noreply.github.com>
2021-08-10 23:31:56 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+# TOKEN = '1839906920:AAEC2mhPH5Do7q0bYUNDs2f_d7L8OjpPma0'  # ТОКЕН создан с использованием бота BotFather
+TOKEN = '1930827544:AAGglpmI2cDO1W_h5tGintjvCE-xPZSR-Co'  # test_on_my_comp_bot
+secret = 'i3hed87q4iuhg847of8'
+url = 'https://mynamepythonanywhere.pythonanywhere.com/i3hed87q4iuhg847of8'
