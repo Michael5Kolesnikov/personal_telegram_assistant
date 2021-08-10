2518d40368abcee11116ecd226fa1fbe91b53e96
Michael5Kolesnikov <86573517+Michael5Kolesnikov@users.noreply.github.com>
2021-08-10 23:36:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-# TOKEN = '1839906920:AAEC2mhPH5Do7q0bYUNDs2f_d7L8OjpPma0'  # ТОКЕН создан с использованием бота BotFather
-TOKEN = '1930827544:AAGglpmI2cDO1W_h5tGintjvCE-xPZSR-Co'  # test_on_my_comp_bot
-secret = 'i3hed87q4iuhg847of8'
-url = 'https://mynamepythonanywhere.pythonanywhere.com/i3hed87q4iuhg847of8'
+import os
+
+TOKEN = os.environ.get('TOKEN')  # ТОКЕН создан с использованием бота BotFather
+secret = os.environ.get('secret')
+url = 'https://your_host.com/' + secret
