e512c8a156887f9a0dc9b719644f62fd33149c0a
Michael5Kolesnikov <86573517+Michael5Kolesnikov@users.noreply.github.com>
2021-08-20 21:28:20 +0300

    Update remain.py

--- remain.py
+++ remain.py
@@ -1,18 +1,18 @@
 import os
 from datetime import datetime
-import telebot
 
-bot = telebot.TeleBot('1930827544:AAGglpmI2cDO1W_h5tGintjvCE-xPZSR-Co')
+import telebot
+from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
 
 while True:
     n = f'{datetime.now()}'
-    for i in os.listdir('remain'):
+    for i in os.listdir('Remain'):
         now = n[0:4] + n[5:7] + n[8:10] + n[11:13] + n[14:16]
         if int(i[:-4]) <= int(now):
-            f = open(f'remain//{i}')
+            f = open(f'Remain//{i}')
             chat_id = f.readline()
             bot.send_message(chat_id, 'Напоминание о событии: \n' + f.read())
             f.close()
-            os.remove(f'remain//{i}')
-        # if not os.listdir('remain'):
-        #     exit()
+            os.remove(f'Remain//{i}')
